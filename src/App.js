c69d82b57c9f3cf3b45480d11983523050f13221
Aashrith001 <v.aashrith22@gmail.com>
2021-05-17 16:54:40 +0530

    added simple react routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Switch,Route} from 'react-router-dom'
 
 function App() {
-  return (
-      <h1>hello</h1>
-  );
+  return <Switch>
+
+      <Route path="/" exact>
+          <h1>This is home page</h1>
+      </Route>
+      <Route path="/lock" exact>
+          <h1>This is locked page</h1>
+      </Route>
+      <Route>
+          <h1>Error 404</h1>
+          <h4> Page not found</h4>
+      </Route>
+
+  </Switch>
 }
 
 export default App;
