2efc96b3298cec44af051f08392ec7ed4a02c9ec
Aashrith001 <v.aashrith22@gmail.com>
2021-05-17 17:14:02 +0530

    Sample pages setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import {Switch,Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Lock from './pages/Lock';
 
 function App() {
-  return <Switch>
-
-      <Route path="/" exact>
-          <h1>This is home page</h1>
-      </Route>
-      <Route path="/lock" exact>
-          <h1>This is locked page</h1>
-      </Route>
-      <Route>
+  return (
+    <div>
+      <Navs />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/lock" exact>
+          <Lock />
+        </Route>
+        <Route>
           <h1>Error 404</h1>
           <h4> Page not found</h4>
-      </Route>
-
-  </Switch>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Lock.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Lock = () => {
+  return (
+    <div>
+      <h1>This is locked page</h1>
+    </div>
+  )
+}
+
+export default Lock
