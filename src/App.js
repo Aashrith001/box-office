5150e75aff715443cfe576d31b78682ebec1e6a8
Aashrith001 <v.aashrith22@gmail.com>
2021-05-21 20:16:04 +0530

    styled home page p-1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import {ThemeProvider} from 'styled-components';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import Show from './pages/Show'
 
+
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
+
 function App() {
   return (
+    <ThemeProvider theme={theme}>
       <Switch>
         <Route path="/" exact>
           <Home />
@@ -21,6 +33,7 @@ function App() {
           <h4> Page not found</h4>
         </Route>
       </Switch>
+    </ThemeProvider>
   );
 }
 
